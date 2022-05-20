ea35be0ce4b9f9c888a373656c89d24c298199ea
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-05-20 00:53:41 -0500

    one step closer...

--- api/task/model.js
+++ api/task/model.js
@@ -1,15 +1,13 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
-function getAll(id) {
-    if (!id) {
-        return db('tasks')
-    } else {
-        return db('tasks')
-        .join('projects')
-        // .select('projects')
-        .where('task_id',id)
-    }
+function getAll() {
+    // return db('projects').select('project_name', 'project_description')
+        return db('tasks as t')
+        // .join('projects as p', 'project_resources as D')
+        .join('projects as p', 't.task_id', '=', 'p.project_id')
+        .select('t.task_notes', 't.task_description', 't.task_completed', 'p.project_name', 'p.project_description')
+        // .where('t.task_id', 'p.project_id')
 }
 
 function getTask(id) {
